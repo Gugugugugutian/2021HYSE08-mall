bb05908ad4f830f5531791c5a7534b822bedca85
GUTIAN <529131196@qq.COM>
2024-06-02 18:56:17 +0800

    订单和地址后端完成

--- mallServer/routes/address.js
+++ mallServer/routes/address.js
@@ -9,14 +9,15 @@ var sql = {
     // 创建地址表
     createAddressTable:
         'CREATE TABLE IF NOT EXISTS address (' +
-        'id INT NOT NULL AUTO_INCREMENT, ' +
-        'username VARCHAR(20) NOT NULL, ' +
-        'name VARCHAR(20) NOT NULL, ' +
-        'phone VARCHAR(20) NOT NULL, ' +
-        'address VARCHAR(100) NOT NULL, ' +
+        'id INT NOT NULL AUTO_INCREMENT, ' + // id
+        'username VARCHAR(20) NOT NULL, ' + // 地址对应的用户名
+        'name VARCHAR(20) NOT NULL, ' +     // 姓名
+        'phone VARCHAR(20) NOT NULL, ' +    // 电话
+        'city VARCHAR(20) NOT NULL,' +      // 城市
+        'street VARCHAR(100) NOT NULL, ' + // 街道
         'PRIMARY KEY (id))',
     // 添加地址
-    addAddress: 'INSERT INTO address (username, name, phone, address) VALUES (?, ?, ?, ?)',
+    addAddress: 'INSERT INTO address (username, name, phone, city, street) VALUES (?, ?, ?, ?, ?)',
     // 根据用户username获取地址
     getAddressByUsername: 'SELECT * FROM address WHERE username = ?',
     // 根据id删除地址
@@ -24,7 +25,9 @@ var sql = {
 }
 
 // 创建地址表
-router.get('/createTable', function (req, res, next) {
+// url: /address
+// params: none.
+router.get('/', function (req, res, next) {
     pool.getConnection(function (err, connection) {
         connection.query(sql.createAddressTable, function (err, result) {
             if (err) {
@@ -44,14 +47,16 @@ router.get('/createTable', function (req, res, next) {
 });
 
 // 添加地址
+// url: /address/add
+// params: username, name, phone, city, street.
 router.post('/add', function (req, res, next) {
     pool.getConnection(function (err, connection) {
-        connection.query(sql.addAddress, [req.body.username, req.body.name, req.body.phone, req.body.address], function (err, result) {
+        connection.query(sql.addAddress, [req.body.username, req.body.name, req.body.phone, req.body.city, req.body.street], function (err, result) {
             if (err) {
                 console.log(err.message);
                 res.status(500).send({
                     code: 1,
-                    msg: '添加地址失败'
+                    msg: '添加地址失败' + err.message,
                 })
             } else {
                 res.status(200).send({
@@ -66,6 +71,8 @@ router.post('/add', function (req, res, next) {
 
 
 // 根据用户username获取地址
+// url: /address/get
+// params: username
 router.get('/get', function (req, res, next) {
     pool.getConnection(function (err, connection) {
         connection.query(sql.getAddressByUsername, req.query.username, function (err, result) {
@@ -76,7 +83,7 @@ router.get('/get', function (req, res, next) {
                     msg: '获取地址失败'
                 })
             } else if (result.length === 0) {
-                res.status(404).send({
+                res.status(200).send({
                     code: 0,
                     data: [],
                     msg: '该用户没有地址'
@@ -93,8 +100,18 @@ router.get('/get', function (req, res, next) {
 });
 
 // 用户删除地址
-router.delete('/deletes', function (req, res, next) {
+// 每个地址有一个独立的id，通过ID进行删除
+// url: /address/delete
+// params: id
+router.delete('/delete', function (req, res, next) {
     pool.getConnection(function (err, connection) {
+        // if(!req.session.username) {
+        //     res.status(401).send({
+        //         code: 1,
+        //         msg: '请先登录'
+        //     });
+        //     return;
+        // }
         connection.query(sql.deleteAddress, req.query.id, function (err, result) {
             if (err) {
                 console.log(err.message);
--- mallServer/routes/order.js
+++ mallServer/routes/order.js
@@ -6,7 +6,147 @@ var dbConfig = require('../database/db');
 var pool = mysql.createPool(dbConfig.mysql);
 
 var sql = {
-
+    // 创建订单表
+    createOrderTable:
+        'create table if not exists order_table(' +
+        'id int primary key auto_increment,' + // id
+        'username varchar(20),' +       // 下单用户
+        'date varchar(20),' +           // 下单日期
+        'total varchar(20),' +          // 订单总价
+        'address varchar(100),' +       // 地址
+        'orderTime varchar(20),' +      // 下单时间
+        'completionTime varchar(20)' + // 付款时间
+        ')',
+    // 创建订单明细表
+    createOrderItemTable:
+        'create table if not exists order_item_table(' +
+        'orderId int not null, ' +      // 订单id
+        'name varchar(20),' +           // 商品名称
+        'quantity int,' +               // 商品数量
+        'price varchar(20)'+           // 商品最终价格
+        ')',
+    // 创建订单
+    createOrder:
+        'insert into order_table(username,date,total,address,orderTime,completionTime) values(?,?,?,?,?,?)',
+    // 创建订单明细
+    createOrderItem:
+        'insert into order_item_table(orderId,name,quantity,price) values(?,?,?,?)',
+    // 查询订单
+    queryOrder:
+        'select * from order_table where username=?',
+    // 查询订单明细
+    queryOrderItem:
+        'select name,quantity,price from order_item_table where orderId=?',
 }
+
+// 创建订单和订单明细表
+// url: /orders/
+router.get('/', function (req, res, next) {
+    pool.getConnection(function (err, connection) {
+        connection.query(sql.createOrderTable, function (err, result) {
+            if (err) {
+                console.log(err.message);
+            }
+        });
+        connection.query(sql.createOrderItemTable, function (err, result) {
+            if (err) {
+                console.log(err.message);
+            }
+        });
+        res.status(200).send('创建完成');
+        connection.release();
+    });
+});
+
+// 创建订单
+// url: /orders/create
+// params:  username, date, total, address, orderTime,
+//          completionTime, goods(Array)
+router.post('/create', function (req, res, next) {
+    const username = req.body.username;
+    const date = req.body.date;
+    const total = req.body.total;
+    const address = req.body.address;
+    const orderTime = req.body.orderTime;
+    const completionTime = req.body.completionTime;
+    const goods = req.body.goods;
+    console.log(req.body);
+    pool.getConnection(function (err, connection) {
+        connection.query(sql.createOrder, [username, date, total, address, orderTime, completionTime], function (err, result) {
+            if (err) {
+                console.log(err.message);
+            } else {
+                var orderId = result.insertId;
+                for (var i = 0; i < goods.length; i+=3) {
+                    var name = req.body.goods[i];
+                    var quantity = req.body.goods[i+1];
+                    var price = req.body.goods[i+2];
+                    connection.query(sql.createOrderItem, [orderId, name, quantity, price], function (err, result) {
+                        if (err) {
+                           console.log(err.message);
+                        }
+                    });
+                }
+                res.status(200).send({
+                    msg: '创建订单成功',
+                    orderId: orderId,
+                });
+           }
+        });
+    });
+});
+
+function getOrderItem(orderId) {
+    console.log(orderId);
+    return new Promise((resolve, reject) => {
+        pool.getConnection(function (err, connection) {
+            connection.query(sql.queryOrderItem, [orderId], function (err, result) {
+                if (err) {
+                    console.log(err.message);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    });
+}
+// 根据用户名查询订单
+// url: /orders/query
+// params: username
+router.get('/query', function (req, res, next) {
+    let order = [];
+    return new Promise((resolve, reject) => {
+        // 根据用户名获得所有订单
+        const username = req.query.username;
+        pool.getConnection(function (err, connection) {
+            connection.query(sql.queryOrder, [username], function (err, result) {
+                if (err) {
+                    console.log(err.message);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }).then(result => {
+        res.status(200).send({
+            msg: '查询订单成功',
+            order: result,
+        });
+    });
+});
+
+// 根据订单id查询订单明细
+// url: /orders/queryitem
+router.get('/queryitem', function (req, res, next) {
+    const orderId = req.query.orderId;
+    getOrderItem(orderId).then((result) => {
+        res.status(200).send({
+            msg: '查询订单明细成功',
+            orderItem: result,
+        });
+    });
+});
 
 module.exports = router;
